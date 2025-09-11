12eea870e89c84f23e8ae7bddd5f9468bd4a4ae5
Sidney Bösch <sidney.boesch@gmail.com>
2025-09-11 11:10:36 +0200

    remove duration from pdf, link phone numbers

--- src/Service/FinancialSupportExportService.php
+++ src/Service/FinancialSupportExportService.php
@@ -1197,7 +1197,7 @@ class FinancialSupportExportService
                 $contactParts[] = '<a href="mailto:'.$contact['email'].'">'.htmlspecialchars($contact['email']).'</a>';
             }
             if (!empty($contact['phone'])) {
-                $contactParts[] = htmlspecialchars($contact['phone']);
+                $contactParts[] = '<a href="tel:'.$contact['phone'].'">'.htmlspecialchars($contact['phone']).'</a>';
             }
             if (!empty($contact['website'])) {
                 $url = $this->normalizeUrl($contact['website']);
